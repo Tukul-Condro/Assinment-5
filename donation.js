d32278ab80e11be4e144c9a82668cc28e4f7d497
tukulcondro <tukuldas68@gmail.com>
2024-09-25 02:52:01 +0600

    filup challenges

--- donation.js
+++ donation.js
@@ -1,40 +1,26 @@
-document.getElementById('donate-for-noakhali').addEventListener('click',function(event){
+document.getElementById('noakhali').addEventListener('click',function(event){
     event.preventDefault();
-    const donateValue = getInputValueById('donate-noakhali');
-    const balance = getInnerTextById('balance')
-    if(isNaN(donateValue)){
-        alert('Inable to donate')
-        return;
-    }
-    if(donateValue < balance){
-        const newBalance = balance - donateValue;
-    const noakhali = getInnerTextById('balance-noakhali')
-    const netBalance = noakhali + donateValue;
-    const date = new Date();
-    document.getElementById('balance').innerText = newBalance;
-    document.getElementById('balance-noakhali').innerText = netBalance;
-    // document.getElementById('date').innerHTML = date ;
-    
-    const p = document.createElement('p');
-    p.classList.add( "ml-5" ,"pt-5","pl-5" ,"pb-8","border-base-300", "border" ,"rounded-md" ,"space-y-2");
-    p.innerHTML = `
-    <h1>${donateValue} Taka is donated fot Donate for Flood at Noakhali, Bangladesh</h1>
-     <p>Date:${date}</p>
-    `
-    document.getElementById('transaction').appendChild(p);
-        
-    
-    alert('Donation successfull');
-    document.getElementById('donate-success').classList.remove('hidden');
-    document.getElementById('donate-done').addEventListener('click',function(){
-        document.getElementById('donate-success').classList.add('hidden');
-    })
-    }
-    else{
-        alert('unable to donate');
-    }
-    
-    
+
+    donationValue('inpute-noakhali','balance','donate-noakhali','transaction','place-noakhali');
+  
+})
+document.getElementById('feni').addEventListener('click',function(event){
+    event.preventDefault();
+
+    donationValue('input-feni','balance','donate-feni','transaction','place-feni');
+       
+})
+document.getElementById('quota').addEventListener('click',function(event){
+    event.preventDefault();
+
+    donationValue('input-quota','balance','donate-quota','transaction','quoata-aid');
+       
+})
+document.getElementById('comilla').addEventListener('click',function(event){
+    event.preventDefault();
+
+    donationValue('input-comilla','balance','donate-comilla','transaction','place-comilla');
+       
 })
 
 document.getElementById('donation-btn').addEventListener('click',function(){
--- utility.js
+++ utility.js
@@ -1,21 +1,72 @@
 function getInputValueById(id){
-    const inputValue = document.getElementById(id).value;
-    const inputNumber = parseFloat(inputValue);
-    return inputNumber;
+    return document.getElementById(id).value;
 }
 
 function getInnerTextById(id){
-    const textValue = document.getElementById(id).innerText;
-    const textNumber = parseFloat(textValue);
-    return textNumber;
+    return document.getElementById(id).innerText;
 }
+
 function showSectionById(id){
     document.getElementById('donate-data').classList.add('hidden');
     document.getElementById('show-transaction').classList.add('hidden');
     document.getElementById(id).classList.remove('hidden');
 }
+
 function addAndRemoveBgById(id1,id2){
     document.getElementById(id1).classList.add('bg-green-400');
     document.getElementById(id2).classList.remove('bg-green-400','hover:bg-green-400');
    
 }
+
+function setInnerTextById(id, value){
+    document.getElementById(id).innerText = value;
+}
+
+function donationValue(donateValueId,balanceId,netDonateId,historyId,placeId){
+
+    const inputValue = getInputValueById(donateValueId);
+    const donateValue = parseFloat(inputValue);
+    const balance = parseFloat(getInnerTextById(balanceId));
+    const netDonate = parseFloat(getInnerTextById(netDonateId));
+    const placeName = getInnerTextById(placeId);
+
+    if(isNaN(donateValue)){
+        alert('Invalid  Amount')
+        return;
+    }
+    if(donateValue < 0){
+        alert('Unable to Donate')
+        return;
+    }
+    if(donateValue < balance){
+        const newBalance = balance - donateValue;
+        const netDoneteBalance = netDonate + donateValue;
+
+        const date = new Date();
+        setInnerTextById(balanceId, newBalance);
+        setInnerTextById(netDonateId, netDoneteBalance);
+
+        const p = document.createElement('p');
+        p.classList.add( "ml-5" ,"pt-5","pl-5" ,"pb-8","border-base-300", "border" ,"rounded-md" ,"space-y-2");
+        p.innerHTML = `
+        <h1>${donateValue} Taka ${placeName}</h1>
+        <p>Date:${date}</p>
+        `
+        alert('Donation successfull');
+        document.getElementById(historyId).appendChild(p);
+
+        function showModalById(showId,hiddenId){
+            document.getElementById(showId).classList.remove('hidden');
+            document.getElementById(hiddenId).addEventListener('click',function(){
+            document.getElementById(showId).classList.add('hidden');
+           
+            })
+        }
+        showModalById('donate-success','donate-done');
+    }
+    else{
+        alert('You have not enough Money!')
+    }
+    
+}
+
